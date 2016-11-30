42d9c5a1071986593f5e98b938e1b6b898f0d45c
PharrellWANG <15113029g@connect.polyu.hk>
2016-11-30 13:52:18 +0800

    Initial Commit

---
+++ PSNR/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PSNR
+//
+//  Created by Pharrell_WANG on 30/11/2016.
+//  Copyright © 2016 Pharrell_WANG. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
